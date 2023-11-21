c6df85ba27b397e68fbedac159f2cd4be2651dfe
Giampiero015 <150815405+Giampiero015@users.noreply.github.com>
2023-11-21 18:59:36 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TS3
+Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
---
+++ TS3.js
@@ -0,0 +1,6 @@
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `
+Hello, ${name} .
+${message}`
+console.log(greeting);
